a4617bb200d47e5496e7c21ba2e226a49acb4af7
MASUD-01 <masudrana96141@gmail.com>
2022-06-13 14:36:01 +0600

    add carousel

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,14 +1,57 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 import './Banner.css'
+import banner1 from '../../asset/dantist-banner.jpg'
+import banner2 from '../../asset/banner2.jpeg'
+import banner3 from '../../asset/banner3.jpg'
 
 const Banner = () => {
     return (
-        <div className='banner-div row m-0 mx-auto'>
-            <div className='p-3'>
-                <img width='100%' height='350px' src='https://i.ibb.co/PjT2b3L/Screenshot-6.png' alt="" />
-                <h1 className='banner text-white'><span className='display-1'>Clean</span> Your Teeth</h1>
-            </div>
-        </div>
+        // <div className='banner-div row m-0 mx-auto'>
+        //     <div className='p-3'>
+        //         <img width='100%' height='350px' src='https://i.ibb.co/PjT2b3L/Screenshot-6.png' alt="" />
+        //         <h1 className='banner text-white'><span className='display-1'>Clean</span> Your Teeth</h1>
+        //     </div>
+        // </div>
+
+        <Carousel fade>
+            <Carousel.Item>
+                {/* <h1 className='banner text-white'><span className='display-1'>Clean</span> Your Teeth</h1> */}
+                <img
+                    className="d-block w-100"
+                    src={banner3}
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner1}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src={banner2}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
     );
 };
 
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,7 @@ import { Container, Nav, Navbar } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import auth from '../../firebase.init';
+import logo from '../../asset/png.png'
 import './Header.css'
 
 const Header = () => {
@@ -12,19 +13,24 @@ const Header = () => {
         signOut(auth)
     }
     return (
-        <Navbar collapseOnSelect expand="lg" variant="dark" style={{ background: '#0d6efd' }} >
+        <Navbar collapseOnSelect expand="lg" variant="light" style={{ background: '#ffffff' }} >
             <Container>
-                <Navbar.Brand as={Link} to="/home" className='fs-1'>Dantist Care</Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Brand as={Link} to="/home" className='fs-1 text-black'> <img className='image-size' src={logo} alt="logo" /> Dentist Care</Navbar.Brand>
+                <Navbar.Toggle className='toggle' aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="ms-auto nav-text" >
                         <Nav.Link as={Link} to="/home">Home</Nav.Link>
-                        <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
                         <Nav.Link as={Link} to="/aboutme">About me</Nav.Link>
+                        <Nav.Link as={Link} to="">Our Team</Nav.Link>
+                        <Nav.Link as={Link} to="">pages</Nav.Link>
+                        <Nav.Link as={Link} to="">service</Nav.Link>
+                        <Nav.Link as={Link} to="">contact</Nav.Link>
+                        <Nav.Link as={Link} to="/blogs">Blogs</Nav.Link>
                         {!user ? <Nav.Link as={Link} to="/login">Login</Nav.Link> :
                             <Nav.Link as={Link} onClick={handleSignout} to="/home">Signout</Nav.Link>}
                         <Nav.Link as={Link} to="/register">Register</Nav.Link>
                     </Nav>
+                    <Nav.Link as={Link} to="" className='appoinment'>Appoinment</Nav.Link>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
